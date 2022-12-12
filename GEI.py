30a230004f04345692649336ee6dfbab42c5a808
glawton1 <george_lawton@brown.edu>
2022-12-12 17:19:34 -0500

    add human centering

--- GEI.py
+++ GEI.py
@@ -1,9 +1,10 @@
 import numpy as np
+from preprocess import preprocess
 
 def create_GEI(train_imgs, val_imgs):
     #TODO: prepocess images to get centered figures
-    train_imgs_pre = np.array()
-    val_imgs_pre = np.array()
+    train_imgs_pre = preprocess(train_imgs)
+    val_imgs_pre = preprocess(val_imgs)
 
     train_data = np.array((train_imgs_pre.shape[0], train_imgs_pre.shape[1], train_imgs_pre.shape[2]))
     val_data = np.array((val_imgs_pre.shape[0], val_imgs_pre.shape[1], val_imgs_pre.shape[2]))
