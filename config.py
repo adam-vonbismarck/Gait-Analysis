d553a6d666a7f80ffb1d7e944c5e043da8a869b1
Mathijs Deetman <mathijs_deetman@brown.edu>
2022-12-14 01:55:15 -0500

    remove path data

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Parameters:
     """
     This class contains the paths to the data + other constants
     """
-    casia_b_path = '/Users/mdeetman/Projects/GaitDatasetB-silh'
-    videos_path = '/Users/mdeetman/Projects/CassiaD'
+    casia_b_path = ''
+    videos_path = ''
 
     preprocess_image_height = 210
     preprocess_image_width = 70
