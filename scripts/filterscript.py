c423c0fa0b0d0fc935d68cae62b1f55e083228ee
adam-vonbismarck <adam_von_bismarck@brown.edu>
2022-12-14 10:12:44 -0500

    add filter script

--- scripts/filterscript.py
+++ scripts/filterscript.py
@@ -1,8 +1,9 @@
 import os
 import shutil
-import config
 
-for root, dirs, files in os.walk(config.Parameters.casia_b_path):
+path = ''
+
+for root, dirs, files in os.walk(path):
     for dir in dirs:
-        if len(dir) == 3 and dir != "090" and len(root) > len(config.Parameters.casia_b_path) + 5:
+        if len(dir) == 3 and dir != "090" and len(root) > len(path) + 5:
             shutil.rmtree(os.path.join(root, dir))
